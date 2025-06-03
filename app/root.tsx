044cedf38692cc27a3b1a520c72a732af54e66f2
Chi-Han Huang <huangchihan@gmail.com>
2025-06-03 11:10:23 +0800

    Able to render tailwind with remix properly.

--- app/root.tsx
+++ app/root.tsx
@@ -5,10 +5,16 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
 import { Analytics } from "@vercel/analytics/react";
 import { LanguageProvider } from "@/contexts/LanguageContext";
-import { CartProvider } from "./contexts/CartContext";
-import "./tailwind.css";
+import { CartProvider } from "@/contexts/CartContext";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import styles from "./tailwind.css?url"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -31,10 +37,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return (
-    <LanguageProvider>
-      <CartProvider>
-        <Outlet />
-      </CartProvider>
-    </LanguageProvider>
+    <TooltipProvider>
+      <LanguageProvider>
+        <CartProvider>
+          <Outlet />
+        </CartProvider>
+      </LanguageProvider>
+    </TooltipProvider>
   );
 }
