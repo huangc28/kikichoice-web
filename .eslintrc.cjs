0025809dd55d7e5e35e474f0c96f4083fba0797d
Chi-Han Huang <huangchihan@gmail.com>
2025-06-03 14:03:32 +0800

    Minor fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,8 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
+  root: true,
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  rules: {
+    "import/no-duplicates": "off"
+  }
 };
